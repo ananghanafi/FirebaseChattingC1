1b39689afe94e39c2fbc5446a2c7a2ded9af2e48
gin <ginanjar1103@gmail.com>
2018-08-30 03:04:21 +0700

    Progress interface,chat,notif beres. R error

--- app/src/androidTest/java/com/masbie/firebasechattingc1/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/masbie/firebasechattingc1/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.masbie.firebasechattingc1", appContext.getPackageName());
+        Assert.assertEquals("com.masbie.firebasechattingc1", appContext.getPackageName());
     }
 }
