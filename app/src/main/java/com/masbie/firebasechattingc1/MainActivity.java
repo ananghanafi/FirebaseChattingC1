1444ab627f03d5e6a3955b4cda5764bd76e2b34d
gin <ginanjar1103@gmail.com>
2018-08-30 12:34:45 +0700

    Progress interface,chat,notif

--- app/src/main/java/com/masbie/firebasechattingc1/MainActivity.java
+++ app/src/main/java/com/masbie/firebasechattingc1/MainActivity.java
@@ -33,6 +33,8 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView mMessages;
     private FirebaseRecyclerAdapter<ChatModel, ChatHolder> mRecycleViewAdapter;
 
+
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
